af871518fb27ff34f82bd740ed252c6c41846eb2
Erfan <erfantolou@gmail.com>
2026-06-27 13:59:39 +0330

    Refactor next.config.js to remove unused image domains and headers configuration

--- next.config.js
+++ next.config.js
@@ -2,32 +2,6 @@
 const nextConfig = {
   images: {
     unoptimized: true,
-    domains: ["localhost", "reyhansmart.ir"],
-  },
-  output: "standalone",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "https://reyhansmart.ir" : "",
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/video/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
   },
 };
 
